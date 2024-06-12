7575ac36223f5aca0442993eb596fe1918048636
patricia-resende <patricia.res17@gmail.com>
2024-06-12 20:07:31 -0300

    Variável Int e Variável Byte

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        byte variavelByte = -127;
+        //short -32768 a 32767
+        //char 0 a 65535
+        //int -2bil a 2bil
+        //long -9t a 9t
+
+        int variavelInt = 26789098;
+        System.out.println(variavelByte);
+        System.out.println(variavelInt);
+
     }
 }
